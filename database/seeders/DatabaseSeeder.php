c5775ff33eae219e8b0ca5811f7149a23eef5d01
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-05-13 13:00:18 +0200

    install debugbar in app

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\Job::factory(100)->create();
+        
         // \App\Models\User::factory(10)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',

d09984f0dd0f991ce9530dd5f2e6fee596643e4b
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-05-17 14:05:35 +0200

    jobs page: Tag component and job info

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::get('', fn() => to_route('jobs.index'));
 Route::resource('jobs', JobController::class)
     ->only('index');
 

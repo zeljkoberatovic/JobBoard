b18a217d5c9c655299fa0d0b76edc5161159a3ee
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-10-08 21:17:25 +0200

    FilteringJobs-RadioButtonFilters

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -18,7 +18,33 @@
             <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" />
           </div>
         </div>
-        <div>3</div>
+        <div>
+          <div class="mb-1 font-semibold">Experience</div>
+          <label for="experience" class="mb-1 flex items-center">
+            <input type="radio" name="experience" value=""
+              @checked(!request('experience')) />
+            <span class="ml-2">All</span>
+          </label>
+
+          <label for="experience" class="mb-1 flex items-center">
+            <input type="radio" name="experience" value="entry"
+              @checked('entry' === request('experience')) />
+            <span class="ml-2">Entry</span>
+          </label>
+
+          <label for="experience" class="mb-1 flex items-center">
+            <input type="radio" name="experience" value="intermediate"
+              @checked('intermediate' === request('experience')) />
+            <span class="ml-2">Intermediate</span>
+          </label>
+
+          <label for="experience" class="mb-1 flex items-center">
+            <input type="radio" name="experience" value="senior"
+              @checked('senior' === request('experience')) />
+            <span class="ml-2">Senior</span>
+          </label>
+        </div>
+        </div>
         <div>4</div>
       </div>
       <button class="w-full">Filter</button>

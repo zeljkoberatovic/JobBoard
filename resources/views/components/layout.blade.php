1643fa649cfda4e978f7210b9e0d6a3c53abd0f1
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-10-18 11:45:32 +0200

    Plain Vanilla JavaScript to Alpine.js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,18 +5,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Job Board</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <title>Job Board</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    </head>
 
-        <body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-blue-300 via-blue-200 to-gray-300 text-slate-900">
+    <body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-blue-300 via-blue-200 to-gray-300 text-slate-900">
+        {{ $slot }}
+    </body>
 
-
-            {{ $slot }}
-
-            
-        </body>
 </html>

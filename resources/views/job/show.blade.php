dfc0ab5b82e3f1b10dc1be7a2b2fcbfbe6c247b6
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-10-21 14:00:29 +0200

    the job policy

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,13 +1,16 @@
 <x-layout>
 
-  <x-breadcrumbs class="mb-4"
+<x-breadcrumbs class="mb-4"
     :links="['Jobs' => route('jobs.index'), $job->title => '#']"/>
 
-    <x-job-card :$job >
+  <x-job-card :$job >
           <p class="mb-4 text-sm text-slate-500">
             {!! nl2br(e($job->description)) !!}
           </p>
-    </x-job-card>
+    <x-link-button :href="route('job.application.create', $job)">
+            Apply
+    </x-link-button>
+  </x-job-card>
 
     <x-card class="mb-4">
       <h2 class="mb-4 text-lg font-medium">

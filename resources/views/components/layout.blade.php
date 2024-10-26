d5b628a07cdd3321e36d7a3c28cee8a5cb35ac34
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-10-26 11:56:34 +0200

    refactor flash errors

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Job Board</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-        <!-- Fonts -->
+      <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     </head>
@@ -22,7 +20,6 @@
                 <a href="{{ route('jobs.index') }}">Home</a>
               </li>
             </ul>
-        
             <ul class="flex space-x-2">
               @auth
                 <li>
@@ -37,25 +34,32 @@
                     <button>Logout</button>
                   </form>
                 </li>
-              @else
+               @else
                 <li>
                   <a href="{{ route('auth.create') }}">Sign in</a>
                 </li>
               @endauth
             </ul>
-          </nav>
+        </nav>
           
           <!--flash poruka-->
           @if (session('success'))
-          <div role="alert"
-            class="my-8 rounded-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
-            <p class="font-bold">Success!</p>
-            <p>{{ session('success') }}</p>
-          </div>
-        @endif
+            <div role="alert"
+              class="my-8 rounded-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
+              <p class="font-bold">Success!</p>
+              <p>{{ session('success') }}</p>
+            </div>
+          @endif
+
+          @if (session('error'))
+            <div role="alert"
+              class="my-8 rounded-md border-l-4 border-red-300 bg-red-100 p-4 text-red-700 opacity-75">
+              <p class="font-bold">Error!</p>
+              <p>{{ session('error') }}</p>
+            </div>
+          @endif
 
 
         {{ $slot }}
-    </body>
-
+      </body>
 </html>

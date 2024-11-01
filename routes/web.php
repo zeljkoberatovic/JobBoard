d83f3b8169bd91e99c3193d4381a999157048b62
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-11-01 13:43:01 +0100

    ...

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,9 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\MyJobApplicationController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+
+
 Route::get('', fn() => to_route('jobs.index'));
 
 Route::resource('jobs', JobController::class)

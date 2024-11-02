104b339398458806fbaf7ae84892519ac8d1ad4a
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-11-02 11:54:08 +0100

    soft deletes-queryng for trashedRecords

--- app/Http/Controllers/MyJobController.php
+++ app/Http/Controllers/MyJobController.php
@@ -18,6 +18,7 @@ class MyJobController extends Controller
                 'jobs' => auth()->user()->employer
                     ->jobs()
                     ->with(['employer', 'jobApplications', 'jobApplications.user'])
+                    ->withTrashed()
                     ->get()
             ]
         );
